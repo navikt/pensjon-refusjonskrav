384e156ac3f93465e9ecbb4688d2c4f7ca9eac14
Pål Fleischer Børjeson <paalfb@yahoo.se>
2024-10-14 14:47:24 +0200

    PES-548: More tests.
    Co-authored-by: Jens Chr. Madsen <Jens.Christian.Madsen@nav.no>
    Co-authored-by: Mathias Sand Jahren <the.mather1@gmail.com>

--- src/test/kotlin/no/nav/pensjon/refusjonskrav/controller/RefusjonskravControllerTest.kt
+++ src/test/kotlin/no/nav/pensjon/refusjonskrav/controller/RefusjonskravControllerTest.kt
@@ -16,6 +16,7 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
+import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
 
 @AutoConfigureMockMvc
@@ -44,9 +45,76 @@ class RefusjonskravControllerTest {
             contentType = MediaType.APPLICATION_JSON
             content = requestJson
         }.andDo { print() } .andExpect {
-            status { isOk() }
+            status {
+                isOk()
+            }
             content { contentType(MediaType.APPLICATION_JSON) }
         }
     }
 
+    @Test
+    fun `refusjonskrav allerede registrert gir http status conflict`() {
+        val request = Refusjonskrav("12345678901", "3010", 1234L, true, emptyList())
+
+        val requestJson = jacksonObjectMapper().writeValueAsString(request)
+
+        //samRestTemplate.postForEntity("/api/refusjonskrav", refusjonskrav, OpprettRefusjonskravResponse::class.java).body!!
+        every { samRestTemplate.postForEntity("/api/refusjonskrav/", request, OpprettRefusjonskravResponse::class.java)  } returns
+                ResponseEntity<OpprettRefusjonskravResponse>(OpprettRefusjonskravResponse(true, Exception("Message with id = ${request.samId} has already been answered or time limit is exceeded."), "SamAlreadyAnsweredOrTimeLimitExceededException"), HttpStatus.OK)
+
+        mockMvc.post("/api/refusjonskrav/") {
+            contentType = MediaType.APPLICATION_JSON
+            content = requestJson
+        }.andDo { print()
+        } .andExpect {
+            status {
+                isConflict()
+                reason("Message with id = 1234 has already been answered or time limit is exceeded.")
+            }
+        }
+    }
+
+    @Test
+    fun `refusjonskrav allerede registrert gir http status internal server error`() {
+        val request = Refusjonskrav("12345678901", "3010", 1234L, true, emptyList())
+
+        val requestJson = jacksonObjectMapper().writeValueAsString(request)
+
+        //samRestTemplate.postForEntity("/api/refusjonskrav", refusjonskrav, OpprettRefusjonskravResponse::class.java).body!!
+        every { samRestTemplate.postForEntity("/api/refusjonskrav/", request, OpprettRefusjonskravResponse::class.java)  } returns
+                ResponseEntity<OpprettRefusjonskravResponse>(OpprettRefusjonskravResponse(false, Exception("No tpforhold exist"), "NoTPForholdExistException"), HttpStatus.OK)
+
+        mockMvc.post("/api/refusjonskrav/") {
+            contentType = MediaType.APPLICATION_JSON
+            content = requestJson
+        }.andDo { print()
+        } .andExpect {
+            status {
+                isInternalServerError()
+                reason("No tpforhold exist")
+            }
+        }
+    }
+
+    @Test
+    fun `refusjonskrav unexpected exception mot sam`() {
+        val request = Refusjonskrav("12345678901", "3010", 1234L, true, emptyList())
+
+        val requestJson = jacksonObjectMapper().writeValueAsString(request)
+
+        //samRestTemplate.postForEntity("/api/refusjonskrav", refusjonskrav, OpprettRefusjonskravResponse::class.java).body!!
+        every { samRestTemplate.postForEntity("/api/refusjonskrav/", request, OpprettRefusjonskravResponse::class.java)  } throws
+                RestClientException("Unexpected exception")
+
+        mockMvc.post("/api/refusjonskrav/") {
+            contentType = MediaType.APPLICATION_JSON
+            content = requestJson
+        }.andDo { print()
+        } .andExpect {
+            status {
+                isInternalServerError()
+                reason("Unexpected exception")
+            }
+        }
+    }
 }
