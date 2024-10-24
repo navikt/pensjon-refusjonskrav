925051114590e20c0719602ce842732c155a7987
Pål Fleischer Børjeson <paalfb@yahoo.se>
2024-10-24 16:11:03 +0200

    PES-548: Sikret med maskinporten og entraID.
    Co-authored-by: Mathias Sand Jahren <the.mather1@gmail.com>

--- src/main/kotlin/no/nav/pensjon/refusjonskrav/controller/RefusjonskravController.kt
+++ src/main/kotlin/no/nav/pensjon/refusjonskrav/controller/RefusjonskravController.kt
@@ -2,6 +2,8 @@ package no.nav.pensjon.refusjonskrav.controller
 
 import no.nav.pensjon.refusjonskrav.domain.Refusjonskrav
 import no.nav.pensjon.refusjonskrav.service.SamClient
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.api.RequiredIssuers
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequiredIssuers(
+    ProtectedWithClaims(issuer = "maskinporten", claimMap = ["scope=nav:pensjon:refusjonskrav"]),
+    ProtectedWithClaims(issuer = "entraID")
+)
 class RefusjonskravController(private val samClient: SamClient) {
 
     private val logger : Logger = LoggerFactory.getLogger(javaClass)
