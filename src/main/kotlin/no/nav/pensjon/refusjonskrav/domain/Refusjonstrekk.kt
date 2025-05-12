a7896ed1993aa5fd609916d349578d89d8ec8f19
TheMather1 <the.mather1@gmail.com>
2025-05-12 14:30:07 +0200

    PES-548: Write Refusjonstrekk dates as millis.

--- src/main/kotlin/no/nav/pensjon/refusjonskrav/domain/Refusjonstrekk.kt
+++ src/main/kotlin/no/nav/pensjon/refusjonskrav/domain/Refusjonstrekk.kt
@@ -1,10 +1,13 @@
 package no.nav.pensjon.refusjonskrav.domain
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
 data class Refusjonstrekk(
     var belop: Double,
     val kravstillersRef: String,
+    @get:JsonFormat(shape = JsonFormat.Shape.NUMBER)
     val datoFom: LocalDateTime,
+    @get:JsonFormat(shape = JsonFormat.Shape.NUMBER)
     val datoTom: LocalDateTime,
 )
