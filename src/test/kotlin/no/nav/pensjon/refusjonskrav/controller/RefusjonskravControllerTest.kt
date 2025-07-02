3a72d7fc0f5ed9ff35505d6212cf7b544406d9c1
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2025-07-02 16:24:04 +0200

    Lagt til ping samt få tester å kjøre, fix.

--- src/test/kotlin/no/nav/pensjon/refusjonskrav/controller/RefusjonskravControllerTest.kt
+++ src/test/kotlin/no/nav/pensjon/refusjonskrav/controller/RefusjonskravControllerTest.kt
@@ -20,7 +20,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
@@ -39,7 +38,7 @@ internal class RefusjonskravControllerTest {
     @Autowired
     protected lateinit var server: MockOAuth2Server
 
-    @SpykBean
+    @MockkBean
     private lateinit var azureM2MTokenInterceptor: AzureM2MTokenInterceptor
 
     @MockkBean
