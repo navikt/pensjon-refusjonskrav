6d5ee710b3c562941220d35028e45fd0c46c3ed8
TheMather1 <the.mather1@gmail.com>
2026-03-10 11:29:21 +0100

    Add logging and timeout to MaskinportenValidator

--- src/main/kotlin/no/nav/pensjon/refusjonskrav/config/MaskinportenValidator.kt
+++ src/main/kotlin/no/nav/pensjon/refusjonskrav/config/MaskinportenValidator.kt
@@ -2,6 +2,8 @@ package no.nav.pensjon.refusjonskrav.config
 
 import com.nimbusds.jwt.JWT
 import com.nimbusds.jwt.JWTClaimsSet
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.http.HttpStatus.FORBIDDEN
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClientException
 import org.springframework.web.client.getForEntity
 import org.springframework.web.server.ResponseStatusException
+import java.time.Duration.ofSeconds
 
 @Service
 class MaskinportenValidator(
@@ -16,12 +19,14 @@ class MaskinportenValidator(
     @Value("\${tp.url}") tpUrl: String
 ) {
 
-    private val restTemplate = RestTemplateBuilder().rootUri(tpUrl).build()
+    private val restTemplate = RestTemplateBuilder().rootUri(tpUrl).readTimeout(ofSeconds(10)).build()
+    private val log: Logger = LoggerFactory.getLogger(javaClass)
 
     fun validateTpnrAuthorization(tpnr: String, token: JWT) {
         token.jwtClaimsSet.apply {
             if (issuer == maskinportenIssuer) {
                 try {
+                    log.info("Maskinporten token received. Validating tpnr: $tpnr is managed by orgno: $orgno")
                     restTemplate.getForEntity<Boolean>("/api/tpconfig/organisation/validate/${tpnr}_$orgno")
                 } catch (e: RestClientException) {
                     throw ResponseStatusException(FORBIDDEN, "tpnr: $tpnr is not managed by orgno: $orgno", e)
