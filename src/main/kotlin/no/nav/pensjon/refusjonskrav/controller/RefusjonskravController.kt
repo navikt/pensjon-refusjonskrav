a670980c0a7285bae9b84aa4666ec762896fb381
TheMather1 <the.mather1@gmail.com>
2026-06-16 13:07:50 +0200

    Cleanup rebase.

--- src/main/kotlin/no/nav/pensjon/refusjonskrav/controller/RefusjonskravController.kt
+++ src/main/kotlin/no/nav/pensjon/refusjonskrav/controller/RefusjonskravController.kt
@@ -1,15 +1,13 @@
 package no.nav.pensjon.refusjonskrav.controller
 
-import com.nimbusds.jwt.JWTParser
 import jakarta.validation.Valid
-import no.nav.pensjon.refusjonskrav.config.MaskinportenValidator
 import no.nav.pensjon.refusjonskrav.domain.Refusjonskrav
 import no.nav.pensjon.refusjonskrav.service.RefusjonskravService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.security.token.support.core.api.RequiredIssuers
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
@@ -26,16 +24,17 @@ class RefusjonskravController(
 
     @Validated
     @PostMapping("/api/refusjonskrav")
+    @ResponseStatus(NO_CONTENT)
     fun opprett(
         @Valid @RequestBody refusjonskrav: Refusjonskrav,
         @RequestAttribute(required = false) orgno: String?
-    ): ResponseEntity<Unit> {
+    ) {
         refusjonskravService.behandleRefusjonskrav(refusjonskrav, orgno)
-        return ResponseEntity.noContent().build()
-
     }
 
     @GetMapping("/api/ping")
-    fun ping(): ResponseEntity<Boolean> = ResponseEntity.ok().body(true)
-        .also { logger.info("Ping utført") }
+    fun ping(): Boolean {
+        logger.info("Ping utført")
+        return true
+    }
 }
